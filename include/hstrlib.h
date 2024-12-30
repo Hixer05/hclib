df6511e70077a8f84afc4a25080566e7d3e6e001
William <williambrusa@airmail.cc>
2024-12-30 20:20:37 +0100

    hstrlib, strlen

---
+++ include/hstrlib.h
@@ -0,0 +1,8 @@
+#ifndef HSTRLIB_H_
+#define HSTRLIB_H_
+
+#include<stddef.h>
+
+const size_t strlen(const char*const str);
+
+#endif // HSTRLIB_H_
---
+++ src/hstrlib.c
@@ -0,0 +1,8 @@
+#include "../include/hstrlib.h"
+
+const size_t strlen(const char *const str){
+    size_t i = 0;
+    for(; str[i]!='\0'; i++)
+
+    return i;
+}
